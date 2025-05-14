b8a6227eea753ee2b9cd7fd0de90379383a80e3f
Evelyn_Tr <linht.nca@gmail.com>
2025-05-13 23:03:27 -0500

    Update query.py

--- src/models/query.py
+++ src/models/query.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
 
+#class RAGRequest(BaseModel):
+#    question: str
 class RAGRequest(BaseModel):
     question: str
-
+    num_responses: int = 2
+    
 class RAGResponse(BaseModel):
     answers: List[str]
