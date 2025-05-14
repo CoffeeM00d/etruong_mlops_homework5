092673711d199c70bb4602d15288988ff7f32cb6
Evelyn_Tr <linht.nca@gmail.com>
2025-05-13 23:47:11 -0500

    Update query.py

--- src/api/query.py
+++ src/api/query.py
@@ -6,9 +6,9 @@ router = APIRouter()
 
 @router.post("/similar_responses", response_model=RAGResponse)
 def get_similar_responses(request: RAGRequest):
-        if request.with_scores:
-        results = retriever.get_similarity_score(request.question)
-    else:
-        results = retriever.get_similar_responses(request.question)
+        #if request.with_scores:
+        #results = retriever.get_similarity_score(request.question)
+    #else:
+    results = retriever.get_similar_responses(request.question)
     return {"answers": results[:request.num_responses]}
     #return RAGResponse(answers=results)
